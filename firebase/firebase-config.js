fcf988c5911d39db1ebae1e167faec7f611a1a93
sitedepostagens <brawlstars14062008@gmail.com>
2025-07-14 19:49:25 -0300

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -2,14 +2,14 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-auth.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDU5ubRhulfZtNcv7MkKNoMy7xTu9l2WUE",
-  authDomain: "login-sistema-c46ff.firebaseapp.com",
-  projectId: "login-sistema-c46ff",
-  storageBucket: "login-sistema-c46ff.appspot.com",
-  messagingSenderId: "356267105486",
-  appId: "1:356267105486:web:afe409f8d23993eeaa449f",
-  measurementId: "G-ND9YWZVVZ7"
+  apiKey: "AIzaSyCMTSLhey7MMtXy_xV2oSuPe18Tl7sLucY",
+  authDomain: "asenha-project-site.firebaseapp.com",
+  projectId: "asenha-project-site",
+  storageBucket: "asenha-project-site.firebasestorage.app",
+  messagingSenderId: "393532682519",
+  appId: "1:393532682519:web:d16a441d5428c676128752",
+  measurementId: "G-38CG9H4XV8"
 };
 
-const app = initializeApp(firebaseConfig);
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
